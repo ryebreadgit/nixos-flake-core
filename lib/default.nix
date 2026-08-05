7ffed3919fadb746acfd754a5d4f16c8ff3ae965
RyeBread <rye@rye.moe>
2026-08-04 20:41:03 -0600

    Initial commit

---
+++ lib/default.nix
@@ -0,0 +1,36 @@
+{ lib }:
+rec {
+  mkApp =
+    { category, categoryCfg, guiCfg }:
+    { description, needsGui ? false }:
+    lib.mkOption {
+      type = lib.types.bool;
+      default = categoryCfg.enable && (!needsGui || guiCfg.enable);
+      defaultText = lib.literalExpression (
+        "config.myConfig.${category}.enable"
+        + lib.optionalString needsGui " && config.myConfig.gui.enable"
+      );
+      example = false;
+      inherit description;
+    };
+
+  # Declares `<app>.enable` + `<app>.package` for every entry in `apps`.
+  mkSimpleOptions =
+    { category, categoryCfg, guiCfg, pkgs, apps }:
+    lib.mapAttrs
+      (name: spec: {
+        enable = mkApp { inherit category categoryCfg guiCfg; } {
+          inherit (spec) description;
+          needsGui = spec.gui or false;
+        };
+        package = lib.mkPackageOption pkgs (spec.pkg or name) { };
+      })
+      apps;
+
+  # The HM half: one `home.packages` entry per enabled app.
+  mkSimplePackages =
+    { categoryCfg, apps }:
+    lib.concatMap
+      (name: lib.optional categoryCfg.${name}.enable categoryCfg.${name}.package)
+      (lib.attrNames apps);
+}
---
+++ nixos/options.nix
@@ -0,0 +1,16 @@
+{ lib, ... }:
+{
+  options.myConfig = {
+    gui.enable = lib.mkEnableOption "graphical applications" // { default = true; };
+
+    users = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+      example = [ "rye" ];
+      description = ''
+        Accounts that category modules grant supplementary groups and
+        privileged access to (wireshark, pcap, polkit owners, and so on).
+      '';
+    };
+  };
+}
